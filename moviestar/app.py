658059636938730ed7475595255caee8c87b6954
yeopto <gunyeop530@gmail.com>
2022-02-03 00:33:42 +0900

    2022-02-03 create-api 중복추가 불가하도록 수정, 그에 맞춰 html파일 스크립트 태그 수정

--- moviestar/app.py
+++ moviestar/app.py
@@ -27,17 +27,20 @@ def create_stars():
   name = soup.select_one('#content > div.main_pack > section.sc_new > div.cm_top_wrap > div.title_area > h2 > span > strong').text
   img_url = soup.select_one('#content > div.main_pack > section.sc_new > div.cm_content_wrap > div._cm_content_area_profile > div.cm_info_box > div.detail_info > a > img')['src']
   recent_work = soup.select_one('#content > div.main_pack > section.sc_new > div.cm_content_wrap > div.cm_content_area > div._cm_content_area_work > div.cm_info_box > div.scroll_box > div.list_info > div > ul.list > li > div.area_card > div.title_box > strong > a').text
+  exist = db.mystar.find_one({'name' : name})
+  if exist is None:
+    doc = {
+      'name': name,
+      'img_url': img_url,
+      'recent': recent_work,
+      'url': url_receive,
+      'like': 0 
+    }
 
-  doc = {
-    'name': name,
-    'img_url': img_url,
-    'recent': recent_work,
-    'url': url_receive,
-    'like': 0 
-  }
-
-  db.mystar.insert_one(doc)
-  return jsonify({'result': 'success'})
+    db.mystar.insert_one(doc)
+    return jsonify({'result': 'success'})
+  else:
+    return jsonify({'result': 'error'}) 
 
 #Read
 @app.route('/api/list', methods=['GET'])
