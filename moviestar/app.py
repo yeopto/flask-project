137ebe004aee9ff5eaa833ec3a14fe777f0e5ef0
yeopto <gunyeop530@gmail.com>
2022-02-02 19:44:20 +0900

    2022-02-02 moviestar finish

--- moviestar/app.py
+++ moviestar/app.py
@@ -1,5 +1,7 @@
-from crypt import methods
 from pymongo import MongoClient
+
+import requests
+from bs4 import BeautifulSoup
 
 from flask import Flask, render_template, jsonify, request
 
@@ -12,11 +14,38 @@ db = client.dbtest
 def home():
   return render_template('index.html')
 
+#Create
+@app.route('/api/create', methods=['POST'])
+def create_stars():
+  name_receive = request.form['name_give']
+  url_receive = f'https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query={name_receive}&oquery={name_receive}&tqi=hQo3ylp0J1sssnNj42ZssssssSR-383698'
+  
+  headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+  data = requests.get(url_receive, headers=headers)
+  soup = BeautifulSoup(data.text, 'html.parser')
+  
+  name = soup.select_one('#content > div.main_pack > section.sc_new > div.cm_top_wrap > div.title_area > h2 > span > strong').text
+  img_url = soup.select_one('#content > div.main_pack > section.sc_new > div.cm_content_wrap > div._cm_content_area_profile > div.cm_info_box > div.detail_info > a > img')['src']
+  recent_work = soup.select_one('#content > div.main_pack > section.sc_new > div.cm_content_wrap > div.cm_content_area > div._cm_content_area_work > div.cm_info_box > div.scroll_box > div.list_info > div > ul.list > li > div.area_card > div.title_box > strong > a').text
+
+  doc = {
+    'name': name,
+    'img_url': img_url,
+    'recent': recent_work,
+    'url': url_receive,
+    'like': 0 
+  }
+
+  db.mystar.insert_one(doc)
+  return jsonify({'result': 'success'})
+
+#Read
 @app.route('/api/list', methods=['GET'])
 def show_stars():
   stars = list(db.mystar.find({}, {'_id': False}).sort('like', -1))
   return jsonify({'result': 'success', 'stars_list': stars})
 
+#Update
 @app.route('/api/like', methods=['POST'])
 def like_stars():
   name_receive = request.form['name_give']
@@ -26,6 +55,7 @@ def like_stars():
   db.mystar.update_one({'name': name_receive}, {'$set': {'like': new_like}})
   return jsonify({'result': 'success'})
 
+#Update
 @app.route('/api/hate', methods=['POST'])
 def hate_stars():
   name_receive = request.form['name_give']
@@ -38,16 +68,12 @@ def hate_stars():
     db.mystar.update_one({'name': name_receive}, {'$set': {'like': new_like}})
     return jsonify({'result': 'success'})
 
+#Delete
 @app.route('/api/delete', methods=['POST'])
 def delete_stars():
   name_receive = request.form['name_give']
   db.mystar.delete_one({'name': name_receive})
   return jsonify({'result': 'success'})
-
-@app.route('/api/create', methods=['POST'])
-def create_stars():
-  return 
-  
 
 if __name__ == '__main__':
   app.run('0.0.0.0', port=5000, debug=True)
